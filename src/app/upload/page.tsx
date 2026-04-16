8cf35537313a9b3e9eaf4881abae578059b3db9a
ishantswami13-crypto <ishantswami13@gmail.com>
2026-04-16 11:19:43 +0530

    fix: add force-dynamic to upload page to prevent build-time DB call

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import UploadWorkspace from "@/components/upload/UploadWorkspace";
 import { getDefaultOrganizationProfile } from "@/lib/organization-profile";
